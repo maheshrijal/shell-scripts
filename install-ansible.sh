086741b399f45eccbee011c5c1e02f1ab337651b
maheshrjl <62394512+maheshrjl@users.noreply.github.com>
2022-03-01 10:01:33 +0530

    install ansible on RedHat & Debian

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,38 +1,54 @@
-#! /bin/bash
+#!/bin/bash
+if [[ `which yum` ]]; then
+   IS_RHEL=1
+   echo "System is RHEL, using yum to install pip3"
+   sudo yum check-update
 
-echo "Update apt cache"
+   echo "Installing ssh & python3"
+   yum install -y sshpass
+   yum install -y python3
 
-sudo apt update
+   echo "Installinig pip"
+   yum install -y python3-pip
 
-echo "Updating apt packages"
+elif [[ `which apt` ]]; then
+   IS_UBUNTU=1
+   echo "System is Debian, using apt for installing pip3"
+   echo "updating apt cache"
+   sudo apt update
+   
+   echo "Updating apt packages"
+   sudo apt -y upgrade
+   echo "Installing latest python version.."
+   echo ""
+   sudo apt install python3
+   sudo apt install sshpass
+   echo "Installing pip3"
+   echo ""
+   sudo apt install python3-pip
 
-sudo apt -y upgrade
+else
+   IS_UNKNOWN=1
+   echo "Script works on debain or RedHat based systems!"
+   echo "Exit!"
+   exit 0
+fi
 
-echo "Install latest python version.."
-
-sudo apt install python
-pythonver="$(python -V 2>&1)"
-
-echo "Install pip3"
-
-sudo apt install python3-pip
-
-pipver="$(pip3 -V 2>&1)"
-
-echo "Install ansible for user"
-
+echo "Installing ansible for user"
 python -m pip install --user ansible
 
+echo "Checking versions of installed software:"
+echo ""
+pythonver="$(python -V 2>&1)"
+pipver="$(pip3 -V 2>&1)"
 ansiblever="$(ansible --version 2>&1)"
 
-python -m pip install --user paramiko
-
-echo "Check if ansible installed correctly"
+echo "Ansible sanity test.."
 echo ""
 ansible localhost -m ping
 echo ""
+
 echo "Python: $pythonver"
 echo "Pip: $pipver"
 echo "Ansible: $ansiblever"
-echo ""
 echo "End!"
