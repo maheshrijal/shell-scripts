d288ad125301943f653f898cd5c633feb7b692df
Mahesh Rijal <62394512+maheshrjl@users.noreply.github.com>
2023-01-19 20:34:19 +0530

    added version support
    Now a specific ansible version can be installed. Eg: ./ansible.sh 2.10

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 set -e
 if [[ $(which yum) ]]; then
-   echo "System is RHEL, using yum to install pip3"
-   sudo yum check-update
+    echo "System is RHEL, using yum to install pip3"
+    sudo yum check-update
 
-   echo "Installing ssh & python3"
-   yum install -y sshpass
-   yum install -y python3
+    echo "Installing ssh & python3"
+    yum install -y sshpass
+    yum install -y python3
 
-   echo "Installinig pip"
-   yum install -y python3-pip
+    echo "Installinig pip"
+    yum install -y python3-pip
 
 elif [[ $(which apt) ]]; then
-   echo "System is Debian, using apt for installing pip3"
-   echo "updating apt cache"
-   sudo apt update
-   
-   echo "Updating apt packages"
-   sudo apt -y upgrade
-   echo "Installing latest python version.."
-   echo ""
-   sudo apt install python3
-   sudo apt install sshpass
-   echo "Installing pip3"
-   echo ""
-   sudo apt install python3-pip
+    echo "System is Debian, using apt for installing pip3"
+    echo "updating apt cache"
+    sudo apt update
+
+    echo "Installing latest python version.."
+    echo ""
+    sudo apt install python3
+    sudo apt install sshpass
+    echo "Installing pip3"
+    echo ""
+    sudo apt install python3-pip
 
 else
-   echo "Script works on debain or RedHat based systems!"
-   echo "Exit!"
-   exit 0
+    echo "Script works on debain or RedHat based systems!"
+    echo "Exit!"
+    exit 0
 fi
 
-echo "Installing ansible for user"
-python3 -m pip install --user ansible
+if [[ $# -eq 0 ]]; then
+    echo "No version passed, Installing latest version."
+    echo "Installing ansible for user"
+    python3 -m pip install --user ansible
+
+elif [[ $# -eq 1 ]]; then
+    echo "Version passed. Installing ansible==$1"
+    echo "Installing ansible for user"
+    python3 -m pip install --user ansible=="$1"
+fi
 
 echo "Install ansible-lint for user"
 python3 -m pip install --user ansible-lint
@@ -41,7 +47,7 @@ python3 -m pip install --user ansible-lint
 echo "Install yamllint for user"
 python3 -m pip install --user yamllint
 
-if grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null ; then
+if grep -qEi "(Microsoft|WSL)" /proc/version &>/dev/null; then
     echo "On WSL | Exporting ~/.local/bin"
     export PATH=$PATH:~/.local/bin
 fi
@@ -51,8 +57,8 @@ echo ""
 pythonver="$(python3 -V 2>&1)"
 pipver="$(pip3 -V 2>&1)"
 ansiblever="$(ansible --version 2>&1)"
-ansiblelint="$( ansible-lint --version 2>&1)"
-yamllint="$( yamllint --version 2>&1)"
+ansiblelint="$(ansible-lint --version 2>&1)"
+yamllint="$(yamllint --version 2>&1)"
 
 echo "Ansible sanity test.."
 echo ""
